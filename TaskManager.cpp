43b9ff2adfa1ac4e065b0125e79998ad2c937ac9
Omar Mohamed <mmr12420@gmail.com>
2026-09-24 00:47:32 +0300

    Add project files.

---
+++ TaskManager.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "TaskManager.h"
+
+using namespace std;
+
+void ShowTasks()
+{
+    cout << "No tasks available." << endl;
+}
---
+++ TaskManagerApp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "TaskManager.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "=== Task Manager ===" << endl;
+
+    ShowTasks();
+
+    return 0;
+}
